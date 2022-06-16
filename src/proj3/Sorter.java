b38e04d20346209bcd1696cdf0bc491e268c065a
David McConnell <mccodav1@gmail.com>
2022-06-16 19:28:55 -0400

    Adds working version

--- src/proj3/Sorter.java
+++ src/proj3/Sorter.java
@@ -2,7 +2,7 @@
  * CLASS: Sorter (Sorter.java)
  *
  * DESCRIPTION:
- * TODO INSERT DESCRIPTION HERE
+ * Sorts ArrayList of students in ascending order by last name.
  * 
  *
  * COURSE AND PROJECT INFO
@@ -21,27 +21,69 @@ package proj3;
 import java.util.ArrayList;
 
 public class Sorter {
-	/*
-	 *  implement quicksort algorithm
-	 */
-	
-	private static int partition(ArrayList<Student> pList, int pFromIdx, int pToIdx) {
-		//todo
-		return 0;
-	}
-	
-	private static void quickSort(ArrayList<Student> pList, int pFromIdx, int pToIdx) {
-		//todo
-	}
-	
-	public static void sort(ArrayList<Student> pList) {
-		//todo
-	}
-	
-	private void swap(ArrayList<Student> pList, int pIdx1, int pIdx2) {
-		//todo
-	}
-	
-	
-	
+
+    /**
+     * Sorts given ArrayList of students.
+     * 
+     * @param pList List to be sorted
+     */
+    public static void sort(ArrayList<Student> pList) {
+        quickSort(pList, 0, pList.size() - 1);
+    }
+
+    /**
+     * Utilized in quick sort algorithm; partitions list into smaller sublists.
+     * 
+     * @param pList     List to be partitioned
+     * @param fromIndex Beginning index
+     * @param toIndex   Ending index
+     * @return Returns "right" index, as used in recursive quick sort algorithm
+     */
+    private static int partition(ArrayList<Student> pList, int fromIndex, int toIndex) {
+        int leftIndex = fromIndex - 1;
+        int rightIndex = toIndex + 1;
+        Student pivot = pList.get(fromIndex);
+        while (leftIndex < rightIndex) {
+            leftIndex++;
+            while (pList.get(leftIndex).compareTo(pivot) < 0) {
+                leftIndex++;
+            }
+            rightIndex--;
+            while (pList.get(rightIndex).compareTo(pivot) > 0) {
+                rightIndex--;
+            }
+            if (leftIndex < rightIndex) {
+                swap(pList, leftIndex, rightIndex);
+            }
+        }
+        return rightIndex;
+    }
+
+    /**
+     * Recursive quick sort as taught in lecture.
+     * 
+     * @param pList     List to be sorted
+     * @param fromIndex Beginning index
+     * @param toIndex   Ending index
+     */
+    public static void quickSort(ArrayList<Student> pList, int fromIndex, int toIndex) {
+        if (fromIndex >= toIndex)
+            return;
+        int partitionIndex = partition(pList, fromIndex, toIndex);
+        quickSort(pList, fromIndex, partitionIndex);
+        quickSort(pList, partitionIndex + 1, toIndex);
+    }
+
+    /**
+     * Swaps two items in a given list; utilized for quick sort algorithm
+     * 
+     * @param pList      List containing items to be swapped
+     * @param leftIndex  Index of first item to swap
+     * @param rightIndex Index of second item to swap
+     */
+    private static void swap(ArrayList<Student> pList, int leftIndex, int rightIndex) {
+        Student tmp = pList.get(leftIndex);
+        pList.set(leftIndex, pList.get(rightIndex));
+        pList.set(rightIndex, tmp);
+    }
 }
